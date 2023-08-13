2ed1e4ac2bb0cb4ceb6775c6de357b8e768358be
unknown <hughesgeorgeking@gmail.com>
2023-08-13 05:29:52 +0000

    changes

---
+++ js/mail.js
@@ -0,0 +1,25 @@
+// Form Submission to email
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_7vvyazn";
+  const templateID = "template_otlpdjg";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
+
+// Form Submission to email
